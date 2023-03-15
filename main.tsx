72bfc90fbb5398d806eadc5f44b0b4e7beb07b13
qinhaitao <qinhaitaocn@163.com>
2023-03-15 15:52:04 +0800

    feat: avatar

--- main.tsx
+++ main.tsx
@@ -4,14 +4,18 @@ import blog, { ga, redirects, h } from "https://deno.land/x/blog/blog.tsx";
 
 blog({
   title: "Mars' Blog",
-  description: "This is my tech and life blog.",
+  description: "tech and life",
   // header: <header>Your custom header</header>,
   // section: <section>Your custom section</section>,
   // footer: <footer>Your custom footer</footer>,
-  avatar: "https://tva1.sinaimg.cn/large/008vxvgGly1h800yyqprej30qn0qnwgf.jpg",
+  avatar: "https://yun.dui88.com/mars/avatar/WechatIMG15.jpeg",
   avatarClass: "rounded-full",
   author: "Mars",
-
+  links: [
+    { title: "Email", url: "mailto:qinhaitaojob@gmail.com" },
+    { title: "GitHub", url: "https://github.com/marsqinht" },
+    // { title: "Zhihu", url: "https://www.zhihu.com/people/qin-hai-tao" },
+  ],
   // middlewares: [
 
     // If you want to set up Google Analytics, paste your GA key here.
