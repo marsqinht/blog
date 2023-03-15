cb6a30a467fbc032457ea8ad585dff810a52276e
qinhaitao <qinhaitaocn@163.com>
2023-03-15 16:20:32 +0800

    feat: theme

--- main.tsx
+++ main.tsx
@@ -8,9 +8,11 @@ blog({
   // header: <header>Your custom header</header>,
   // section: <section>Your custom section</section>,
   // footer: <footer>Your custom footer</footer>,
-  avatar: "https://yun.dui88.com/mars/avatar/WechatIMG15.jpeg",
+  avatar: "https://yun.dui88.com/mars/avatar/WechatIMG14.jpeg",
   avatarClass: "rounded-full",
   author: "Mars",
+  theme: "light",
+  favicon: "https://yun.dui88.com/mars/avatar/5b6f98b6-b38a-43b8-b080-0e7f0360e6bf.png",
   links: [
     { title: "Email", url: "mailto:qinhaitaojob@gmail.com" },
     { title: "GitHub", url: "https://github.com/marsqinht" },
@@ -18,14 +20,14 @@ blog({
   ],
   // middlewares: [
 
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
+  //   // If you want to set up Google Analytics, paste your GA key here.
+  //   // ga("UA-XXXXXXXX-X"),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
+  //   // If you want to provide some redirections, you can specify them here,
+  //   // pathname specified in a key will redirect to pathname in the value.
+  //   // redirects({
+  //   //  "/hello_world.html": "/hello_world",
+  //   // }),
 
   // ]
 });
